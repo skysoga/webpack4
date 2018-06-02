89930a2a384d313ebc050d24bf1be01e81b19ed8
st <st20180508@gmail.com>
2018-06-02 15:43:10 +0800

    first test

--- src/views/index/SideNav.vue
+++ src/views/index/SideNav.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="product-navbar classify-navbar">
     <div class="tree-header">内容管理</div>
-    <div class="btn-switch"><span class="fa fa-angle-double-right"></span> <span class="fa fa-angle-left"></span></div>
     <div class="accordion-list">
-
+      <ul>
+        <li></li>
+      </ul>
     </div>
   </div>
 </template>
@@ -11,45 +12,7 @@
   export default {
     data() {
       return {
-        data: [{
-          id: 1,
-          label: '一级 1',
-          children: [{
-            id: 4,
-            label: '二级 1-1',
-            children: [{
-              id: 9,
-              label: '三级 1-1-1'
-            }, {
-              id: 10,
-              label: '三级 1-1-2'
-            }]
-          }]
-        }, {
-          id: 2,
-          label: '一级 2',
-          children: [{
-            id: 5,
-            label: '二级 2-1'
-          }, {
-            id: 6,
-            label: '二级 2-2'
-          }]
-        }, {
-          id: 3,
-          label: '一级 3',
-          children: [{
-            id: 7,
-            label: '二级 3-1'
-          }, {
-            id: 8,
-            label: '二级 3-2'
-          }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
+        data: []
       }
     },
 
